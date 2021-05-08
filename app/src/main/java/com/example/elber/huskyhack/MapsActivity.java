60f66531910b72e5ffa75c09fc3ae80754545d60
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2021-05-08 00:16:36 -0700

    Added listener for marker clicks

--- app/src/main/java/com/example/elber/huskyhack/MapsActivity.java
+++ app/src/main/java/com/example/elber/huskyhack/MapsActivity.java
@@ -1,14 +1,18 @@
 package com.example.elber.huskyhack;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -35,16 +39,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    /*
+     * Documentation Resources:
+     * GoogleMap: https://developers.google.com/android/reference/com/google/android/gms/maps/GoogleMap
+     * CameraUpdateFactory: https://developers.google.com/android/reference/com/google/android/gms/maps/CameraUpdateFactory
+     *
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        // see the following for documentation on CameraUpdateFactory:
-        // https://developers.google.com/android/reference/com/google/android/gms/maps/CameraUpdateFactory
+        // Add a marker in the quad and move the camera
         LatLng uwQuad = new LatLng(47.6579251, -122.3078048);
         mMap.addMarker(new MarkerOptions().position(uwQuad).title("Hi Elbert"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(uwQuad));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uwQuad, 17));
+
+        // add a new marker click listener
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(@NonNull Marker marker) {
+                Log.d("wenjalan", "onMarkerClick: " + marker.getTitle());
+                return true;
+            }
+        });
     }
 }
