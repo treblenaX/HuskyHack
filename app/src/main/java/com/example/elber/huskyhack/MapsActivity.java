c26a9711a1da9614a7484c38c19ba0db47f1f076
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2021-05-07 23:35:35 -0700

    Updated project to AndroidX for maps compat. (use Android Studio v4 for further development)

--- app/src/main/java/com/example/elber/huskyhack/MapsActivity.java
+++ app/src/main/java/com/example/elber/huskyhack/MapsActivity.java
@@ -1,6 +1,5 @@
 package com.example.elber.huskyhack;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -9,6 +8,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import androidx.fragment.app.FragmentActivity;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
